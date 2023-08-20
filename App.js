21224ce623bc999062160342cb096a8ada403549
kipyek <deniskipyegon88@gmail.com>
2023-08-20 22:02:38 +0300

    initial

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Linking from "expo-linking";
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const prefix = Linking.makeUrl("/");
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        Home: "home",
+        Settings: "settings",
+      },
+    },
+  };
+
+  // function handleDeepLink(event) {
+  //   let data = Linking.parse(event.url);
+  //   setData(data);
+  // }
+
+  // useEffect(() => {
+  //   Linking.addEventListener("url", handleDeepLink);
+  //   return () => {
+  //     Linking.removeEventListener("url");
+  //   };
+  // }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>
+    //     {data ? JSON.stringify(data) : "App not opened from deep link"}
+    //   </Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
